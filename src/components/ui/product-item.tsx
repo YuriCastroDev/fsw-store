16fdf8f0e13ff9b7e85d3e82a2c6bb4aac631050
YuriCastroDev <yuri.castro.dev@gmail.com>
2023-10-17 22:21:14 -0300

    Corrigindo a tela de produto

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { ProductWithTotalPrice } from "@/helpers/product";
+import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
 
@@ -9,8 +9,8 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+    <div className="flex max-w-[170px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -24,32 +24,33 @@ const ProductItem = ({ product }: ProductItemProps) => {
         />
 
         {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+          <Badge className="absolute left-3 top-3 px-2 py-[2px] ">
             <ArrowDownIcon size={14} /> {product.discountPercentage}%
           </Badge>
         )}
+      </div>
 
-        <div className="flex flex-col gap-1">
-          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-            {product.name}
-          </p>
+      <div className="flex flex-col gap-1">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
 
-          <div className="flex items-center gap-2">
-            {product.discountPercentage > 0 ? (
-              <>
-                <p className="font-semibold">
-                  R${product.totalPrice.toFixed(2)}
-                </p>
-                <p className="text-xs line-through opacity-75">
-                  R${Number(product.basePrice).toFixed(2)}
-                </p>
-              </>
-            ) : (
-              <p className="text-sm font-semibold">
-                R${Number(product.basePrice).toFixed(2)}
+        <div className="flex items-center gap-2 ">
+          {product.discountPercentage > 0 ? (
+            <>
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                R$ {product.totalPrice.toFixed(2)}
               </p>
-            )}
-          </div>
+
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
         </div>
       </div>
     </div>
